2ad331df4cb7aee73e84a86918a990d83cd4d53b
alexhr1404 <154979243+alexhr1404@users.noreply.github.com>
2024-04-30 13:47:26 +0200

    Add files via upload

---
+++ Branch out/Estilos/script.js
@@ -0,0 +1,18 @@
+
+let acc=document.getElementsByClassName('accordion-container');
+let i;
+
+for(i=0; i<acc.length; i++){
+	acc[i].addEventListener("click",function(){
+		this.classList.toggle("active");
+
+		let contenido= this.nextElementSibling;
+		if(contenido.style.display ==="block"){
+			contenido.style.display = "none";
+		}else{
+			contenido.style.display = "block";
+		}
+	});
+}
+
+
