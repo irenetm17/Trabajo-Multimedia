08e8dc3633214ea6952b2a8014473054c7443e8a
Sara <samepae01@gmail.com>
2024-05-11 10:56:02 +0200

    FAQs arreglo código

--- Branch out/Estilos/script.js
+++ Branch out/Estilos/script.js
@@ -1,23 +1,3 @@
- Branch-alex
-
-let acc=document.getElementsByClassName('accordion-container');
-let i;
-
-for(i=0; i<acc.length; i++){
-	acc[i].addEventListener("click",function(){
-		this.classList.toggle("active");
-
-		let contenido= this.nextElementSibling;
-		if(contenido.style.display ==="block"){
-			contenido.style.display = "none";
-		}else{
-			contenido.style.display = "block";
-		}
-	});
-}
-
-
-
 
 
 let acc=document.getElementsByClassName('accordion-container');
@@ -35,7 +15,3 @@ for(i=0; i<acc.length; i++){
 		}
 	});
 }
-
-
-
- main
